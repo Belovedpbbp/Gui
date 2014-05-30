03d8cb27139659cb6942d0ec16c2b3910827864e
Natsuda.w <natsuda.w@entronica.co.th>
2014-05-30 17:48:17 +0700

    1st Commit 1st

---
+++ TestameterProvider/workspace/TestametorProvider/src/main/java/th/co/entronica/apollo/testametorprovider/App.java
@@ -0,0 +1,18 @@
+package th.co.entronica.apollo.testametorprovider;
+
+import javax.swing.ImageIcon;
+import th.co.entronica.apollo.testametorprovider.controller.MasterController;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println(getClass().getResource("/Images/yahoo_1.gif"));
+        MasterController masterController = MasterController.getInstance();
+       
+    }
+}
